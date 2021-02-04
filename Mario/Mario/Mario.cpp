f8561074b6619afc78b91f8a67f02a8b86fb8495
Hmilner1 <72082549+Hmilner1@users.noreply.github.com>
2021-02-04 18:45:19 +0000

    render function

--- Mario/Mario/Mario.cpp
+++ Mario/Mario/Mario.cpp
@@ -5,10 +5,15 @@
 #include "constants.h"
 using namespace std;
 SDL_Window* g_window = nullptr;
+SDL_Renderer* g_renderer = nullptr;
+SDL_Texture* g_texture = nullptr;
 
 bool InitSDL();
 void CLoseSDL();
 bool Update();
+void Render();
+SDL_Texture* LoadTextureFromFile(string path);
+void FreeTexture();
 int main(int argc, char* args[])
 {
 	if (InitSDL())
@@ -48,6 +53,21 @@ bool InitSDL()
 		}
 	}
 	return true;
+	g_renderer = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+	if (g_renderer != nullptr)
+	{
+		int imageFlags = IMG_INIT_PNG;
+		if (!(IMG_Init(imageFlags) & imageFlags))
+		{
+			cout << "SDL_Image could not initialise. Error: " << IMG_GetError();
+			return false;
+		}
+	}
+	else
+	{
+		cout << "Render could not initilise. Error: " << SDL_GetError();
+		return false;
+	}
 }
 void CLoseSDL()
 {
@@ -80,4 +100,33 @@ bool Update()
 	}
 	}
 	return false;
+}
+void Render()
+{
+	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+	SDL_RenderClear(g_renderer);
+	SDL_Rect renderLocation = { 0,0,SCREEN_WIDTH,SCREEN_HEIGHT };
+	SDL_RenderCopyEx(g_renderer, g_texture, NULL, &renderLocation, 0, NULL, SDL_FLIP_NONE);
+	SDL_RenderPresent(g_renderer);
+}
+SDL_Texture* LoadTextureFromFile(string path)
+{
+	SDL_Texture* p_texture = nullptr;
+	SDL_Surface* p_surface = IMG_Load(path.c_str());
+	if (p_surface != nullptr)
+	{
+		//create the texture from the pixels on the surface
+		p_texture = SDL_CreateTextureFromSurface(g_renderer, p_surface);
+		if (p_texture == nullptr)
+		{
+			cout << "Unable to create texture from surface. Error: " << SDL_GetError();
+		}
+		//remove the loaded surface now that we have a texture
+		SDL_FreeSurface(p_surface);
+	}
+	else
+	{
+		cout << "Unable to create texture from surface. Error: " << IMG_GetError();
+	}
+	return p_texture;
 }
