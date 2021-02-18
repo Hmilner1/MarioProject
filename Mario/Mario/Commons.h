8f1eba25b19ae76915915e4127853e7bec1ba1b1
Hmilner1 <72082549+Hmilner1@users.noreply.github.com>
2021-02-18 16:37:10 +0000

    .

--- Mario/Mario/Commons.h
+++ Mario/Mario/Commons.h
@@ -3,12 +3,16 @@ struct Vector2D
 {
 	float x;
 	float y;
-	float initial_x;
-	float initial_y;
 	Vector2D()
 	{
 		x = 0.0f;
 		y = 0.0f;
 	};
+	
+	Vector2D(float initial_x, float initial_y)
+	{
+		x = initial_x;
+		y = initial_y;
+	}
 };
 
---
+++ Mario/Mario/Texture2D.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _TEXTURE2D_H
+#define _TEXTURE2D_H
+class Texture2D
+{
+
+public:
+
+private:
+
+};
+#endif
